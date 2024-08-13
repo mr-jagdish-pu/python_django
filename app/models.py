667327b27f200832b86cf5b38fc1eecc229a7def
jagdish bhai <jagdissghimire@gmail.com>
2024-08-13 09:22:29 +0545

    something

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField
+    address = models.CharField(max_length=20)
+    faculty_choices = (
+        ('BCA','bca')
+        ('BBA','bba')
+        ('MCA','mca')
+
+    )
+    faculty = models.CharField(max_length=10)
